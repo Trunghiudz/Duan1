a4eda6b46a39cd4201506cbb712ae39db20959b6
Trunghiudz <trunghieu2004x7.gmail.com>
2023-11-22 23:33:17 +0700

    update

---
+++ app/Views/Client/dangki.php
@@ -0,0 +1,79 @@
+<!-- <div class="modal login-modal fade" id="user-login" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered">
+<div class="modal-content">
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+<li class="nav-item" role="presentation">
+<button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Registration</button>
+</li>
+</ul>
+<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+<div class="login-registration-form">
+<div class="form-title">
+<h3>Registration</h3>
+</div>
+<form>
+<div class="form-inner mb-25">
+<input type="text" placeholder="User Name *">
+</div>
+<div class="form-inner mb-25">
+<input type="email" placeholder="Email Here *">
+</div>
+<div class="form-inner mb-25">
+<input id="password2" type="password" placeholder="Password *">
+<i class="bi bi-eye-slash" id="togglePassword2"></i>
+</div>
+<div class="form-inner mb-35">
+<input id="password3" type="password" placeholder="Confirm Password *">
+<i class="bi bi-eye-slash" id="togglePassword3"></i>
+</div>
+<a href="#" class="primary-btn1 hover-btn3">Registration</a>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div> -->
+
+
+
+<div class="modal login-modal fade" id="user-login" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered">
+<div class="modal-content">
+<div class="modal-header">
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+<li class="nav-item" role="presentation">
+<a href=""><button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Registration</button></a>
+</li>
+</ul>
+</div>
+<div class="modal-body">
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <div class="login-registration-form">
+                <div class="form-title">
+                    <h3>Registration</h3>
+                </div>
+                <form>
+                    <div class="form-inner mb-35">
+                        <input type="text" placeholder="User name *">
+                    </div>
+                    <div class="form-inner mb-35">
+                        <input type="email" placeholder="Email Here *">
+                    </div>
+                    <div class="form-inner">
+                        <input id="password" type="password" placeholder="Password *">
+                        <i class="bi bi-eye-slash" id="togglePassword"></i>
+                    </div><br><br>
+                    <div class="form-inner">
+                        <input id="confirmpassword" type="password" placeholder="Confirm Password *">
+                        <i class="bi bi-eye-slash" id="togglePassword"></i>
+                    </div><br><br>
+                    <a href="#" class="primary-btn1 hover-btn3">Registration</a>
+                </form>
+            </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
--- app/Views/Client/index.php
+++ app/Views/Client/index.php
@@ -1,10 +1,25 @@
 <?php
     include("header.php");
-    include("login.php");
     include("chitietsp.php");
     include("home.php");
-    include("footer.php");
+    include("footer.php");  
+    if (isset($_GET['act'])) {
+        $act=$_GET['act'];
+        switch ($act) {
+            case 'dangnhap':
+                include "/login.php";
+            break;
+            case 'dangkiii':
+                include "dangki.php";
+            break;
+            
+            default:
+            
+            break;
+        }
+    }else{
     
+    }
 
 ?>
 
